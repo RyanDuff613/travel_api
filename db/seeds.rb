a0efcf59bf82261b23ab3a08e2a2a103d34ab296
Ryan Duff <ryanduff613@gmail.com>
2020-06-08 10:15:14 -0700

    seed database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+class Seed
+
+  def self.begin
+    seed = Seed.new
+    seed.generate_reviews
+  end
+
+  def generate_reviews
+    20.times do |i|
+      review = Review.create!(
+        user_name: Faker::FunnyName.two_word_name,
+        content: Faker::Lorem.sentence(word_count: 18),
+        rating: Faker::Number.between(from: 1, to: 5),
+        country: Faker::WorldCup.team,
+        city: Faker::Nation.capital_city
+      )
+      puts "Review #{i}: Reviewer is #{review.user_name} and review is '#{review.content}'."
+    end
+  end
+end
+
+Seed.begin
